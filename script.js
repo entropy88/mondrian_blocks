f5e5344816b91fb3a0ae97f7e34dca3e5a1afedb
entropy88 <36485631+entropy88@users.noreply.github.com>
2021-12-31 11:54:15 +0200

    blocks now not going outside of viewport
    randomized number of iterations and colors

--- script.js
+++ script.js
@@ -1,74 +1,97 @@
 console.log('script loaded');
 
-let start=document.getElementById('start');
-let container=document.getElementById('container');
+let start = document.getElementById('start');
+let container = document.getElementById('container');
+let numberOfIterationsParagraph = document.getElementById('numberOfIterations');
 console.log(container)
 console.log(start)
 start.addEventListener('click', draw);
 
 
-// let size=10;
-//fib bullshit
-let n1 = 10;
-let n2 = 10;
-let nextSize=0;
-let colors=['red', 'blue','yellow', 'white'];
-let directions=['toTheLeft','toTheBottom' ]
-
 function sleep(ms) {
     return new Promise(
-      resolve => setTimeout(resolve, ms)
+        resolve => setTimeout(resolve, ms)
     );
-  }
-  
-
-async function draw(){
-for (let i=0; i<8; i++){  
-    console.log(n1);
-    let topDistance=n1;
-    nextSize = n1 + n2;
-    n1 = n2;
-    n2 = nextSize;   
-    drawASquare(n1,i,topDistance);
-    await sleep(2000);
-//    drawASquare(i);
-}
 }
 
-function drawASquare(n1,i,topDistance){
-    console.log('draw a square')
-    let square=document.createElement("div");
-    let squareWidth=`${n1}px`;
-    let squareHeight=`${n1}px`;
+
+    //fib bullshit
+    let n1 = 0;
+    let n2 = 10;
+    let nextSize = 0;
+    let colors = ['red', 'blue', 'yellow', 'white'];
+    let directions = ['toTheLeft', 'toTheBottom'];
+
+
+//kill command
+let kill = false;
+
+async function draw() {
+
+    //disable button
+    start.disabled=true;
+    //reset values
+    n1=0;
+    n2=10;
+    nextSize=0;
    
-    let colorIndex=i;
-  
-    //to change colors
-   if(colorIndex>3){
-       colorIndex=i%colors.length;
-       console.log('color is' + colorIndex)
-   }
- 
-    square.style.width=squareWidth;   
-    square.style.height=squareHeight;
-    square.style.backgroundColor=colors[colorIndex];
+//clear container
+container.innerHTML="";
 
-//     //to change direction
-
-    if (i%2==0){
-        square.className='toTheLeft'
-    } else{
-        square.className='toTheBottom';
+    //random number of iterations
+    let iterations = Math.floor(Math.random() * 15) + 1;
+    numberOfIterations.textContent = `You are about to see ${iterations} iterations or as many as your screen allows`
+    for (let i = 0; i < iterations; i++) {
+        console.log(n1);
+        let previous = n1;
+        nextSize = n1 + n2;
+        n1 = n2;
+        n2 = nextSize;
+        if (!kill) {
+            drawASquare(n1, i, previous);
+            await sleep(2000);
+        } else {
+            alert("Out of space!");  
+            start.disabled=false;         
+            return;
+        }
         
-        let property=`${topDistance}px`;
-            square.style.top=property;
+    }
+    start.disabled=false;
+}
+
+function drawASquare(n1, i, previous) {
+
+    let freeWidth=container.clientWidth-n1;
+    let freeHeight=container.clientHeight-(n1+previous);
+
+    if (freeHeight<n1 || freeWidth<n1){
+        kill=true;    
+    
     }  
+  
+        let square = document.createElement("div");
+        let squareWidth = `${n1}px`;
+        let squareHeight = `${n1}px`;
 
-        let p=document.createElement('p');
-    p.textContent=n1;
-    square.appendChild(p)
-       container.appendChild(square); 
+        //get random color
+        let randomColor = Math.floor(Math.random() * colors.length);
 
+        square.style.width = squareWidth;
+        square.style.height = squareHeight;
+        // square.style.backgroundColor=colors[colorIndex];
+        square.style.backgroundColor = colors[randomColor];
 
+   //to change direction
+        if (i % 2 == 0) {
+            square.className = 'toTheLeft'
+        } else {
+            square.className = 'toTheBottom';
+
+            let property = `${previous}px`;
+            square.style.top = property;
+        }
    
+        container.appendChild(square);       
+
 }
